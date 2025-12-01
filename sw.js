c0baf73d567de40d110b80778b027c658f012296
felipedos-ux <felipe.dos@ufpi.edu.br>
2025-12-01 13:38:16 -0300

    chore: add remaining files

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Service Worker for Push Notifications
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activated');
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
